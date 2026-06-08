23022cd4544a2a02d2daf95859b8f1966e7d7c06
Sadwireddy99 <sadwireddy99@gmail.com>
2026-06-08 21:25:24 +0530

    Time: 1 ms (46.97%) | Memory: 56.7 MB (84.79%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+// var removeDuplicates = function(nums) {
+//    let x =0;
+//    for(let i=1 ; i< nums.length ; i++){
+//     if( nums[x] != nums[i]){
+//         x = x+1;
+//         nums[x] = nums[i];
+        
+//     }
+//    }
+//    return x+1;
+//};
+
+
+ var removeDuplicates = function(nums) {
+    let x =0;
+    for(let i=0; i< nums.length ; i++){
+        if(nums[i] !== nums[x]){
+            x += 1; 
+            nums[x] = nums[i];
+        }
+    }
+    return x+1;
+ }
+
+
+
+

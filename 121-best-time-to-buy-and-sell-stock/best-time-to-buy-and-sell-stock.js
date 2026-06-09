ff06e022916f7e6bdae46b843ab4f75d41a9ada2
Sadwireddy99 <sadwireddy99@gmail.com>
2026-06-09 14:21:07 +0530

    Time: 3 ms (59.33%) | Memory: 65.2 MB (52.88%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,63 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// var maxProfit = function(prices) {
+    // let min = prices[0];
+    // let maxProfit = 0;
+    // for(let i=0; i<prices.length; i++){
+    //     if(prices[i]-min >maxProfit){
+    //         maxProfit = prices[i]-min;
+    //     }
+    //     if(prices[i]<min){
+    //         min = prices[i];
+    //     }
+    // }
+    // return maxProfit;
+
+    // let min = prices[0];
+    // let maxProfit =0;
+    // for(let i=1; i< prices.length ; i++){
+    //     if((prices[i]-min) > maxProfit){
+    //         maxProfit = prices[i]-min
+    //     }
+
+    //     if(prices[i] < min){
+    //         min = prices[i];
+    //     }
+    // }
+    // return maxProfit;
+
+// let min =prices[0];
+// let maxProfit =0;
+// for(let i=1;i<prices.length;i++){
+//     if(prices[i] < min ){
+//       min = prices[i];
+//     }
+//     if(prices[i]-min > maxProfit){
+//         maxProfit = prices[i]-min;
+//     }
+// }
+
+// return maxProfit;
+//};
+
+
+
+                        //June 9th 
+var maxProfit = function(prices) {
+    let min = prices[0];
+    let maxProfit = 0;
+
+    for(let i=0 ; i< prices.length ; i++){
+        if( prices [i] < min ){
+            min = prices[i];
+        }
+        if(prices[i]- min > maxProfit){
+            maxProfit = prices[i] - min;
+        }
+    }
+    return maxProfit;
+}
+
+
